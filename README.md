b50b3f87a3e966f4c4367ebe41b873c5e7724c24
Cheng <cheng.tao@hiq.se>
2020-06-23 22:47:23 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+Functions realized:
+    - login with username and password
+    - add, delete save goals and show the list with SQLite database
+    - add, delete search incomes and expenses and show the list with SQLite database
+    - GET request by API
+
+Functions unrealized (future work):
+    - Get the progress after calculation (it seems that in one activity, in onCreate(), you can only call content only once in a DBhelper() or a Volley.newRequestQueue()  )
--- app/src/main/java/com/example/expensetracker/TabActivity.java
+++ app/src/main/java/com/example/expensetracker/TabActivity.java
@@ -1,22 +1,14 @@
 package com.example.expensetracker;
 
-import android.app.AlertDialog;
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Toast;
 
 import com.example.expensetracker.ui.main.SectionsPagerAdapter;
 
@@ -33,29 +25,5 @@ public class TabActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
 
     }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    //Control the menu
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo ) item.getMenuInfo();
-//        switch (item.getItemId()){
-//            case R.id.add_menu:
-//                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
-////                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder();
-//            case R.id.search_menu:
-//                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.exit_menu:
-//                finish();
-//                break;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 
 }
